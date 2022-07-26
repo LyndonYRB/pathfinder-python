129d4ba0e7f4a6f63b620a18fbeadcca57bae55f
Lyndon <lyndon.stluce@gmail.com>
2022-07-26 16:17:21 -0400

    draw wall added

--- pathfinder-python/pathfinder.py
+++ pathfinder-python/pathfinder.py
@@ -1,5 +1,4 @@
 from tkinter import messagebox, Tk
-from turtle import window_height, window_width
 import pygame
 import sys
 
@@ -8,8 +7,8 @@ window_height = 500
 
 window = pygame.display.set_mode((window_width, window_height))
 
-columns = 25
-rows = 25
+columns = 50
+rows = 50
 
 box_width = window_width // columns
 box_height = window_height // rows
@@ -21,6 +20,9 @@ class Box:
     def __init__(self, i, j):
         self.x = i
         self.y = j
+        self.start = False
+        self.wall = False
+        self.target = False
 
     def draw(self, win, color):
         pygame.draw.rect(win, color, (self.x * box_width,
@@ -34,21 +36,52 @@ for i in range(columns):
         arr.append(Box(i, j))
     grid.append(arr)
 
+start_box = grid[0][0]
+start_box.start = True
+
 
 def main():
+    begin_search = False
+    target_box_set = False
+
     while True:
         for event in pygame.event.get():
           # quit
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            # mouse controls
+            elif event.type == pygame.MOUSEMOTION:
+                x = pygame.mouse.get_pos()[0]
+                y = pygame.mouse.get_pos()[1]
+                # draw the wall
+                if event.buttons[0]:
+                    i = x // box_width
+                    j = y // box_height
+                    grid[i][j].wall = True
+                    # set target
+                    if event.buttons[2] and not target_box_set:
+                        i = x // box_width
+                        j = y // box_height
+                        target_box = grid[i][j]
+                        target_box.target = True
+                        target_box_set = True
+            # start algorithm
+            if event.type == pygame.KEYDOWN and target_box_set:
+                begin_search = True
 
         window.fill((0, 0, 0))
 
         for i in range(columns):
             for j in range(rows):
                 box = grid[i][j]
-                box.draw(window, (20, 20, 20))
+                box.draw(window, (50, 50, 50))
+                if box.start:
+                    box.draw(window, (0, 200, 200))
+                if box.wall:
+                    box.draw(window, (90, 90, 90))
+                if box.target:
+                    box.draw(window, (200, 200, 0))
 
         pygame.display.flip()
 
